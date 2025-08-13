d0de726375f62e93b5ab810c30f85de2164c28e1
Purvaja235 <purvajapatil0101@gmail.com>
2025-08-13 16:47:30 +0530

    Solved Cricket World Cup Problem On CodeChef With Difficulty 203

---
+++ CodeChef/Cricket World Cup Qualifier.java
@@ -0,0 +1,23 @@
+Problem: Cricket World Cup Qualifier
+PlatForm: CodeChef
+Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CWC23QUALIF
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	     Scanner sc = new Scanner(System.in);
+	     int a = sc.nextInt();
+	     if( a <= 12) {
+	         System.out.println("No");
+	     }
+	     else{
+	         System.out.println("Yes");
+	     }
+
+	}
+}
